b478af2335e44c45d9772a20628ac36defb2be1e
Artur Pieńkowski <artpienkowski@gmail.com>
2022-09-28 15:03:00 +0200

    add class BookList

--- js/script.js
+++ js/script.js
@@ -1,169 +1,198 @@
-/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars/
-
-{
+{  
     'use strict';
-
+    
     const select = {
-        templateOf: {
-            books: '#template-book',
-            filters: '.filters',
-        },
-
-        listOf: {
-            booklist: '.books-list',
-        },
-
-        containerOf: {
-            image: '.book__image',
-        },
-
-        imageParams: {
-            id: '.book-id'
-        },
-
-        
+      templateOf: {
+        books: '#template-book',
+      },
+    
+      listOf: {
+        booklist: '.books-list',
+        filters: '.filters',
+      },
+    
+      containerOf: {
+        image: '.book__image',
+      },
+    
+      imageParams: {
+        id: '.book-id',
+      },  
     };
-
+        
     const templates = {
-        bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML)
-    };  
-
-    
-    /* write a function to render a booklist in HTML .books-list */
-    function render(){
-
-        /* make a loop for each book from dataSource.books */
-        for(const eachBook of dataSource.books){
-            
-            /* add const ratingBgc and rangeWidth */
-            const ratingBgc =  determineRatingBgc(eachBook.rating);
-            eachBook.ratingBgc = ratingBgc;
-
-            const ratingWidth = eachBook.ratingBgc * 10;
-            eachBook.ratingWidth = ratingWidth;
-
-            /* generate HTML for each book based on template */
-            const generatedHTML = templates.bookTemplate(eachBook);
-            
-            /* create element using utils.createElementFromHTML */
-            const bookDOMElement = utils.createDOMFromHTML(generatedHTML);
-            
-            /* find booklist container */
-            const bookListContainer = document.querySelector(select.listOf.booklist);
-            
-            /* add DOM element to the booklist */
-            bookListContainer.appendChild(bookDOMElement);
-        }
-    }
-
-    render();
-  
-    
-
-    /* write a function initActions to add a book to a favorite list */
-    function initActions(){
-        /* make an empty array with favorite books */
-        const favoriteBooks = [];
-        
-        
-        /* make a const with reference to entire list of images */
-        const imageListContainer = document.querySelector(select.listOf.booklist);
-
-        /* add event listener to the entire list of images */
-        imageListContainer.addEventListener('dblclick', function(event){
-            event.preventDefault();
-
-            /* check if offset Parent of the image contains class .book_image) */
-            if(event.target.offsetParent.classList.contains('book__image')){
-
-                /* find clickedElement */
-                const clickedElement = event.target.offsetParent;
-
-                /* find const with data-id of the image */
-                const bookID = clickedElement.getAttribute('data-id');
-
-                if(!favoriteBooks.includes(bookID)){
-
-                /* add favorite to clicked element */
-                clickedElement.classList.add('favorite');
-                    
-                /* push this book to the favorite books array */
-                favoriteBooks.push(bookID);
-
-                /* if it's alreayd in the array */
-                }else if(favoriteBooks.includes(bookID)){
-
-                    /* remove class favorite */
-                    clickedElement.classList.remove('favorite');
-
-                     /* remove bookID from the array */
-                    const indexOfBookId = favoriteBooks.indexOf(bookID);
-                    favoriteBooks.splice(indexOfBookId, 1);
-                }
-            }
-        });
-        
-        
-        /* make a reference to the filter form */ 
-        const filterContainer = document.querySelector(select.templateOf.filters);
-
-        /* add events listener to the form */
-        filterContainer.addEventListener('click', function(event){
-            /* find const of clickedElement */
-            const clickedElement = event.target;
-
-            /* check if it's a checkbox that was clicked in the whole filters container - event.target */
-            if(event.target.tagName === 'INPUT' && event.target.type === 'checkbox' && event.target.name === 'filter'){
-                
-                /* if checked - push it's value to array */
-                if(clickedElement.checked){
-                    filters.push(clickedElement.value)
-
-                /* if uncheckes - remove from array */
-                } else {
-                    const indexOfFilterID = filters.indexOf(clickedElement.value);
-                    filters.splice(indexOfFilterID, 1);
-                }
-            }
-            console.log(filters);
-            filterBooks();
-            
-        });
+      bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML),
     };
-
-    initActions();
-    const filters = [];
-
-    function filterBooks(){
-        
-        /*for every book */
-        for(const book of dataSource.books){
-            let shouldBeHidden = false;
-
-            /* for every book check if the filter fits the book */
-            for(const filter of filters){
-                if(!book.details[filter]){
-                    shouldBeHidden = true;
-                    break;
-                }
-            }
-
-            /* if the filter fits the book - hide it, if not - make it visible */
-            const bookImageID = document.querySelector(select.containerOf.image + '[data-id="' + book.id + '"]');
-            if(shouldBeHidden){
-                
-                bookImageID.classList.add('hidden');
-                }else{
-
-                bookImageID.classList.remove('hidden');
-            }
-        }
-    }
-
-    function determineRatingBgc(rating){
-
-        let background = '';
+  
+    class BooksList{
+      constructor(){
+        const thisBooksList = this;
+  
+        thisBooksList.initData();
+        thisBooksList.getElements();
+        thisBooksList.render();
+        thisBooksList.initActions();
+        thisBooksList.filterBooks();
+        thisBooksList.determineRatingBgc();
+  
+      }
     
+      initData() {
+        this.data = dataSource.books;
+      }
+  
+      getElements(){
+        const thisBooksList = this;
+    
+        thisBooksList.bookListContainer = document.querySelector(select.listOf.booklist);
+        thisBooksList.imageListContainer = document.querySelector(select.listOf.booklist);
+        thisBooksList.filtersContainer = document.querySelector(select.listOf.filters);
+        thisBooksList.filters = [];
+        thisBooksList.favoriteBooks = [];
+  
+      }
+  
+      /* write a function to render a booklist in HTML .books-list */
+      render(){
+        const thisBooksList = this;
+  
+        /* make a loop for each book from dataSource.books */
+        for(const eachBook of thisBooksList.data){
+  
+          /* add const ratingBgc and rangeWidth */
+          const ratingBgc =  thisBooksList.determineRatingBgc(eachBook.rating);
+          eachBook.ratingBgc = ratingBgc;
+  
+          const ratingWidth = eachBook.ratingBgc * 10;
+          eachBook.ratingWidth = ratingWidth;
+            
+          /* generate HTML for each book based on template */
+          const generatedHTML = templates.bookTemplate(eachBook);
+          
+          /* create element using utils.createElementFromHTML */
+          const bookDOMElement = utils.createDOMFromHTML(generatedHTML);
+          
+          /* add DOM element to the booklist */
+          thisBooksList.bookListContainer.appendChild(bookDOMElement);
+        }
+  
+      }
+      
+      /* write a function initActions to add a book to a favorite list and filter them*/
+      initActions(){
+        const thisBooksList = this;
+    
+           
+            
+        /* add event listener to the entire list of images */
+        thisBooksList.imageListContainer.addEventListener('dblclick', function(event){
+          event.preventDefault();
+    
+                      
+          /* check if offset Parent of the image contains class .book_image) */
+          if(event.target.offsetParent.classList.contains('book__image')){
+    
+            /* find clickedElement */
+            const clickedElement = event.target.offsetParent;
+    
+            /* find const with data-id of the image */
+            const bookID = clickedElement.getAttribute('data-id');
+    
+            if(!thisBooksList.favoriteBooks.includes(bookID)){
+    
+              /* add favorite to clickedelement */
+              clickedElement.classList.add('favorite');
+    
+              /* push this book to the favorite books array */
+              thisBooksList.favoriteBooks.push(bookID);
+    
+              /* if it's alreayd in the array */
+            } else if(thisBooksList.favoriteBooks.includes(bookID)){
+    
+              /* remove class favorite */
+              clickedElement.classList.remove('favorite');
+    
+              /* remove bookID from the array */
+              const indexOfBookID = thisBooksList.favoriteBooks.indexOf(bookID);
+              thisBooksList.favoriteBooks.splice(indexOfBookID, 1);
+            }
+          }
+            
+          console.log(thisBooksList.favoriteBooks);
+        });
+               
+    
+        /* add event listener to filtersContainer */
+        thisBooksList.filtersContainer.addEventListener('click', function(event){
+          // event.preventDefault();
+    
+          /* find const of clickedElement */
+          const clickedElement = event.target;
+    
+          /* check if it's a checkbox that was clicked in the whole filters container - event.target */
+          if(event.target.tagName === 'INPUT' && event.target.type === 'checkbox' && event.target.name === 'filter'){
+    
+            
+            
+            /* if checked - push it's value to array */
+            if(clickedElement.checked){
+              thisBooksList.filters.push(clickedElement.value);
+                  
+              /* if uncheckes - remove from array */
+            } else {
+      
+              /* remove bookID from the array */
+              const indexOfFilterID = thisBooksList.filters.indexOf(clickedElement.value);
+              thisBooksList.filters.splice(indexOfFilterID, 1);
+            }
+          }
+          console.log(thisBooksList.filters);
+          thisBooksList.filterBooks();
+          console.log(thisBooksList.filterBooks);
+        });
+        
+       
+      }
+    
+    
+      
+      /* write function to see if the book should be hiddden or not */
+      filterBooks(){
+        const thisBooksList = this;
+  
+        /* for every book */
+        for(const book of thisBooksList.data){
+          thisBooksList.bookImageID = document.querySelector(select.containerOf.image + '[data-id="' + book.id + '"]');
+          let shouldBeHidden = false;
+  
+          /* for every book check if the filter fits the book - if not, it should be hidden */
+          for(const filter of thisBooksList.filters){
+            if(!book.details[filter]){
+              shouldBeHidden = true;
+              break;
+            }
+          }
+  
+          /* if the filter fits the book - hide it, if not - make it visible */
+       
+          if(shouldBeHidden){
+            thisBooksList.bookImageID.classList.add('hidden');
+          }else{
+            thisBooksList.bookImageID.classList.remove('hidden');
+          }
+  
+        }
+        
+  
+      }
+  
+      
+      determineRatingBgc(rating){
+        const thisBooksList = this;
+        console.log(thisBooksList);
+  
+        let background = '';
+  
         /* prepare backgrounds for different ratings */
         if(rating < 6){
           background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
@@ -176,6 +205,14 @@
         }
         return background;
       }
-
+      
     
-}
+    }
+    const app = {
+      init: function () {
+        new BooksList();
+      },
+    };
+  
+    app.init();
+  }
